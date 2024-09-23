ff78e5698f007abf153ed16bb691d79cfaa49464
JephDelgado <spencedee022@gmail.com>
2024-09-23 10:20:10 -0700

    added modal__opened & close

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,18 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+/* -------------------------------------------------------------------*/
+/*                       ELEMENTS                                     */
+/* -------------------------------------------------------------------*/
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditModalClose = document.querySelector("#modal-close");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+profileEditModalClose.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
